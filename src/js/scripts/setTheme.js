e7b26d41156bcd74e8e57174589caaa482bbe743
millisabel <38396309+millisabel@users.noreply.github.com>
2020-11-20 21:44:49 +0200

    Update 2020-11-20T19:44:42.081Z

--- src/js/scripts/setTheme.js
+++
@@ -1,7 +0,0 @@
-const setTheme = (selector, toggleClass) => {
-    const btn = document.querySelector(selector);
-    btn.addEventListener('click', function(){
-        document.body.classList.toggle(toggleClass);
-        btn.blur();
-    });
-};
